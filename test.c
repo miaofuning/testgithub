0c4bbadd8c8f6324071ea086b81dd88ff28d0dae
miaofuning <1371659454@qq.com>
2019-11-25 23:55:29 -0800

    xxx change

--- test.c
+++ test.c
@@ -4,7 +4,10 @@ int main(int argc, const char *argv[])
 {
 	printf("hello  world\n");
 	printf("develop***********\n");
-printf("gaga");
+    printf("gaga");
+    printf("gaga");
+    printf("gaga");
+    printf("gaga");
 	printf("hello");
 	return 0;
 }
